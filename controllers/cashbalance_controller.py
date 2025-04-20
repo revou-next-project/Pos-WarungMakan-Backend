f13f464c2bf42e948dffdd62c17b7caf44c0a226
samuelindrawijaya <samuelindraw75@gmail.com>
2025-04-20 17:01:20 +0700

    cukuruk

--- controllers/cashbalance_controller.py
+++ controllers/cashbalance_controller.py
@@ -6,12 +6,12 @@ router = APIRouter(prefix="/cashflow", tags=["CashFlow"])
 
 @router.get("/summary")
 async def get_cashflow_summary_controller(
-    period: str = "day",  # default to daily summary
-    page: int = 1,        # default page number
-    limit: int = 10,      # default records per page
-    start_date: str = None,  # optional start date
-    end_date: str = None,    # optional end date
-    transaction_type: str = None,  # filter by 'SALE' or 'EXPENSE'
+    period: str = "day",  # default 
+    page: int = 1,        # default 
+    limit: int = 10,      # default 
+    start_date: str = None,  # optional 
+    end_date: str = None,    # optional 
+    transaction_type: str = None,  # filter
     current_user: dict = Depends(get_current_user)  # check if user is logged in and authorized
 ):
     if current_user["role"] not in ["admin", "cashier"]:
@@ -24,7 +24,7 @@ async def get_cashflow_summary_controller(
             limit=limit,
             start_date=start_date,
             end_date=end_date,
-            transaction_type=transaction_type  # Pass the filter to the service
+            transaction_type=transaction_type 
         )
         return cashflow_data
     except Exception as e:
